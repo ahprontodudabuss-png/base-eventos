d8d9b9c648f8edb412581c054957ae095c3e7dc3
ahprontodudabuss-png <ahprontodudabuss@gmail.com>
2026-07-02 08:04:52 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,11 +4,12 @@
 
 const CONFIG = {
     // URL gerada pelo Google Apps Script (Deploy > Nova implantação > App da Web)
-    // Exemplo: 'https://script.google.com/macros/s/AKfycb.../exec'
     API_URL: 'https://script.google.com/macros/s/AKfycby1U1lQ47RuFKDRdAaJp8AFqqZilgH64y5Yli_P6O14NZVEpbIxbyUb1l-SxiT41lCl/exec',
     
     // Link base do seu formulário Tally (sem os parâmetros ocultos)
-    // Exemplo: 'https://tally.so/r/mKx...'
     TALLY_URL: 'https://tally.so/embed/D4YRyZ?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1',
+    
+    // Google OAuth Client ID
+    // Crie no Google Cloud Console (https://console.cloud.google.com)
     GOOGLE_CLIENT_ID: '1045722589397-lfjm5dqi4d4bcrgfm4tvlfs0h6q97e2h.apps.googleusercontent.com'
 };
