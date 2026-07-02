5b614b979910701e61679fd0a8a859da883047fa
ahprontodudabuss-png <ahprontodudabuss@gmail.com>
2026-07-01 23:19:30 -0300

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@
 const CONFIG = {
     // URL gerada pelo Google Apps Script (Deploy > Nova implantação > App da Web)
     // Exemplo: 'https://script.google.com/macros/s/AKfycb.../exec'
-    API_URL: 'COLOQUE_AQUI_A_URL_DO_SEU_APPS_SCRIPT',
+    API_URL: 'https://script.google.com/macros/s/AKfycby1U1lQ47RuFKDRdAaJp8AFqqZilgH64y5Yli_P6O14NZVEpbIxbyUb1l-SxiT41lCl/exec',
     
     // Link base do seu formulário Tally (sem os parâmetros ocultos)
     // Exemplo: 'https://tally.so/r/mKx...'
-    TALLY_URL: 'COLOQUE_AQUI_O_LINK_DO_SEU_TALLY'
+    TALLY_URL: 'https://tally.so/embed/D4YRyZ?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1'
 };
